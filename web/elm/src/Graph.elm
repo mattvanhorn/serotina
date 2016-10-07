f162aa6ba4100bf11c49f8f44304c8531756ed61
Matt Van Horn <mattvanhorn@gmail.com>
2016-10-07 08:27:20 -0700

    Build a Cartesian graph

--- web/elm/src/Graph.elm
+++ web/elm/src/Graph.elm
@@ -23,3 +23,29 @@ edges : Graph -> List Edge
 edges graph =
     List.map (\node -> Node.edges node) graph.nodes
         |> List.concat
+
+
+cartesian : Int -> Int -> Graph
+cartesian width height =
+    let
+        rangeCheck : Int -> Int -> Bool
+        rangeCheck =
+            \n x ->
+                (0 <= x)
+                    && (x < width * height)
+                    -- right border check
+                    &&
+                        not (x % width == 0 && x == n + 1)
+                    -- left border check
+                    &&
+                        not (n % width == 0 && x == n - 1)
+
+        getNeighbors : Int -> List Int
+        getNeighbors =
+            \idx ->
+                List.filter (rangeCheck idx) [ idx - 1, idx + 1, idx - width, idx + width ]
+    in
+        List.repeat height (List.repeat width 0)
+            |> List.concat
+            |> List.indexedMap (\idx a -> Node idx (getNeighbors idx))
+            |> new
--- web/elm/tests/Tests.elm
+++ web/elm/tests/Tests.elm
@@ -8,17 +8,33 @@ import Node exposing (Node)
 
 all : Test
 all =
-    describe "An undirected graph"
-        [ test "A single node graph has no edges" <|
-            \() ->
-                Graph.new ([ Node 42 [] ])
-                    |> Graph.nodes
-                    |> List.length
-                    |> Expect.equal 1
-        , test "A node with a neighbor has two edges (one for each direction)" <|
-            \() ->
-                Graph.new ([ (Node 1 [ 2 ]), (Node 2 [ 1 ]) ])
-                    |> Graph.edges
-                    |> List.length
-                    |> Expect.equal 2
+    describe "a graph"
+        [ describe "that is undirected"
+            [ test "A single node graph has no edges" <|
+                \() ->
+                    Graph.new ([ Node 42 [] ])
+                        |> Graph.nodes
+                        |> List.length
+                        |> Expect.equal 1
+            , test "A node with a neighbor has two edges (one for each direction)" <|
+                \() ->
+                    Graph.new ([ (Node 1 [ 2 ]), (Node 2 [ 1 ]) ])
+                        |> Graph.edges
+                        |> List.length
+                        |> Expect.equal 2
+            ]
+        , describe "that is cartesian"
+            [ test "a 2 x 2 graph has 4 nodes" <|
+                \() ->
+                    Graph.cartesian 2 2
+                        |> Graph.nodes
+                        |> List.length
+                        |> Expect.equal 4
+            , test "a 2 x 2 graph has 8 edges" <|
+                \() ->
+                    Graph.cartesian 2 2
+                        |> Graph.edges
+                        |> List.length
+                        |> Expect.equal 8
+            ]
         ]
